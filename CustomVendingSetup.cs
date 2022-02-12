39486a9720c02de3d11c65bc9a4796610c767939
WhiteThunder <whetethunger@gmail.com>
2022-02-12 00:59:43 -0800

    Increment minor version

--- CustomVendingSetup.cs
+++ CustomVendingSetup.cs
@@ -19,7 +19,7 @@ using CustomSaveDataCallback = System.Action<Newtonsoft.Json.Linq.JObject>;
 
 namespace Oxide.Plugins
 {
-    [Info("Custom Vending Setup", "WhiteThunder", "2.2.0")]
+    [Info("Custom Vending Setup", "WhiteThunder", "2.3.0")]
     [Description("Allows editing orders at NPC vending machines.")]
     internal class CustomVendingSetup : CovalencePlugin
     {
